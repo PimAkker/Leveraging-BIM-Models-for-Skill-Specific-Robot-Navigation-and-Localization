fa995e35a5255d6a07901e46fe277e782887b0e6
PimAkker <pimjanvandenakker@gmail.com>
2023-05-01 11:38:54 +0200

    ignore

--- ros_workspace/build/rosbot_description/src/rosbot_gazebo/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: ~/ITP_project/ros_workspace/src/rosbot_description/src/rosbot_gazebo
-# Build directory: ~/ITP_project/ros_workspace/build/rosbot_description/src/rosbot_gazebo
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
