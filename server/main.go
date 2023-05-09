ecc314b54df46b8afc7f8e8ae1ee82bacbccc4c2
Marko Macura <m.macura@student.tue.nl>
2023-05-09 09:43:26 -0700

    Make GET and POST api

--- server/main.go
+++ server/main.go
@@ -50,7 +50,60 @@ func main() {
 			l.Fatal(err)
 		}
 	}()
-	sendTTL("atlas_8_floor.ttl")
+
+	// go func() {
+	// 	requestURL := fmt.Sprintf("http://localhost:7200/rest/repositories/Project/")
+	// 	req, err := http.NewRequest(http.MethodGet, requestURL, nil)
+	// 	if err != nil {
+	// 		fmt.Printf("client: could not create request: %s\n", err)
+	// 		os.Exit(1)
+	// 	}
+
+	// 	res, err := http.DefaultClient.Do(req)
+	// 	if err != nil {
+	// 		fmt.Printf("client: error making http request: %s\n", err)
+	// 		os.Exit(1)
+	// 	}
+
+	// 	fmt.Printf("client: got response!\n")
+	// 	fmt.Printf("client: status code: %d\n", res.StatusCode)
+
+	// 	resBody, err := ioutil.ReadAll(res.Body)
+	// 	if err != nil {
+	// 		fmt.Printf("client: could not read response body: %s\n", err)
+	// 		os.Exit(1)
+	// 	}
+	// 	fmt.Printf("client: response body: %s\n", resBody)
+	// }()
+
+	go func() {
+		jsonBody := []byte(`{"element": "Columns"}`)
+		bodyReader := bytes.NewReader(jsonBody)
+
+		requestURL := fmt.Sprintf("http://localhost:7200")
+		req, err := http.NewRequest(http.MethodPost, requestURL, bodyReader)
+
+		if err != nil {
+			fmt.Printf("client: could not create request: %s\n", err)
+			os.Exit(1)
+		}
+
+		res, err := http.DefaultClient.Do(req)
+		if err != nil {
+			fmt.Printf("client: error making http request: %s\n", err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("client: got response!\n")
+		fmt.Printf("client: status code: %d\n", res.StatusCode)
+
+		// resBody, err := ioutil.ReadAll(res.Body)
+		// if err != nil {
+		// 	fmt.Printf("client: could not read response body: %s\n", err)
+		// 	os.Exit(1)
+		// }
+		fmt.Print("client: response body: ", res)
+	}()
 
 	sigChan := make(chan os.Signal)
 	signal.Notify(sigChan, os.Interrupt)
